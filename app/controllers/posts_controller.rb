bffd3dcd72037687b71cbb3668c426d248e378d0
Jaushi <144474840+Jaushi@users.noreply.github.com>
2024-09-30 15:52:01 +0800

    Add: Show action in post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,31 @@
 class PostsController < ApplicationController
     def index
-        @posts = Post.all
+      @posts = Post.all
     end
-
+  
     def new
-        @post = Post.new
+      @post = Post.new
     end
-
+  
     def create
-        @post = Post.new(post_params)
-        if @post.save
-            redirect_to posts_path, notice: 'Post was successfully created.'
-        else
-            flash.now[:alert] = 'Post create failed'
-            render :new, status: :unprocessable_entity
-        end
+      @post = Post.new(post_params)
+      if @post.save
+        redirect_to posts_path, notice: 'Post was successfully created.'
+      else
+        render :new
+      end
     end
-
+  
+    def show
+      @post = Post.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The post you're looking for cannot be found."
+      redirect_to posts_path
+    end
+  
     private
-
+  
     def post_params
-        params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content)
     end
-end
+  end
