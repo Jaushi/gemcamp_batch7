26cd25c5ad3bcae953268dc7cf325e2bd58c8f7f
Jaushi <144474840+Jaushi@users.noreply.github.com>
2024-10-01 10:39:29 +0800

    Add: Edit tab and fixed validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,37 @@
 class PostsController < ApplicationController
-    def index
-      @posts = Post.all
-    end
-  
-    def new
-      @post = Post.new
-    end
-  
-    def create
-      @post = Post.new(post_params)
-      if @post.save
-        redirect_to posts_path, notice: 'Post was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    def show
-      @post = Post.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      flash[:alert] = "The post you're looking for cannot be found."
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post created successfully'
       redirect_to posts_path
-    end
-  
-    private
-  
-    def post_params
-      params.require(:post).permit(:title, :content)
+    else
+      flash.now[:alert] = 'Post create failed'
+      render :new, status: :unprocessable_entity
     end
   end
+
+  def show
+    @post = Post.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The post you're looking for cannot be found."
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
